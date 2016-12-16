e3ee1f56c4cafbb610102c0df5fa402ef34b209a
caromedellin <caromedellin@gmail.com>
2016-12-16 10:59:45 -0500

    Add sata types file

---
+++ 30days/Python/dataTypes.py
@@ -0,0 +1,15 @@
+# Declare second integer, double, and String variables.
+a = raw_input()
+b = raw_input()
+c = raw_input()
+# Read and save an integer, double, and String to your variables.
+a = int(a)
+b = float(b)
+c = str(c)
+# Print the sum of both integer variables on a new line.
+print a + i
+# Print the sum of the double variables on a new line.
+print b + d
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s + c
